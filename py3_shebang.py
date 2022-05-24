4b603490fe1b7e7f9c1566d5faff4bb0f42113e3
Fred <frederick.jackson.iii@verizonwireless.com>
2022-05-24 18:55:36 +0000

    learning about input function

---
+++ py3_shebang.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+print("ello!")
+print("Did you say, hello?")
+print("No, I said ello, but that\'s close enough.")
+#!/bin/rm
+# This script calls on the executable called **rm** (remove), which is
+# installed to the */bin* directory. When this script is run,
+# bash will execute this script with *rm*, which will remove (delete)
+# the script :p
